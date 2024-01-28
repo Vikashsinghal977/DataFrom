ca120a25831a629884d36163baff86e15f996c75
Vikash <vikashsinghal509@gmail.com>
2024-01-28 22:20:30 +0530

    adding function getData

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const {MongoClient} = require("mongodb")
+const {MongoClient} = require("mongodb");
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+function getData(){
+    let result = client.connect();
+}
