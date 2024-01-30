b314622e2685e18cef00985c0c6a3088f7882a04
Vikash <vikashsinghal509@gmail.com>
2024-01-30 15:06:02 +0530

    creating the data MyFlex

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const {MongoClient} = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 
-function getData(){
-    let result = client.connect();
+async function getData(){
+    try{
+        const database = client.db("sample_myflex");
+        const movies = database.collection("movies");
+
+        // ading title 
+        const query = {title:"Money hiest"};
+        const movie = await movies.findOne(query);
+
+        console.log(movie);
+    } finally{
+        await client.close();
+    }
 }
+getData().catch(console.dir);
